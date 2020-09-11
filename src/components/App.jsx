de2117536650f625b68f431418689f9cd909bd96
Evgeniy Kondratyev <kondratiev.evg@gmail.com>
2020-09-11 12:12:25 +0300

    init EK_TODO App

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React  from 'react'
+
+let App = () => {
+    return(
+        <div>
+           
+        </div>
+    )
+}
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode:'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+          {
+            test: /\.jsx?$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env','@babel/preset-react',{
+                  'plugins': ['@babel/plugin-proposal-class-properties', "@babel/plugin-transform-runtime"]}]
+              }
+            }
+          },
+          {
+            test: /\.css$/,
+            use: [
+              {
+                loader: MiniCssExtractPlugin.loader
+              },
+              'css-loader',
+            ],
+          },
+          {
+            test: /\.(png|jpe?g|gif)$/i,
+            use: [
+              {
+                loader: 'file-loader',
+                options: {
+                  outputPath: 'images',
+                },
+              },
+            ],
+          },
+        ]
+      },
+      plugins: [new MiniCssExtractPlugin({
+        filename: 'style.css'
+      })],
+      devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        port: 9000,
+        watchContentBase: true,
+        progress: true,
+        historyApiFallback: true
+      },
+  };
